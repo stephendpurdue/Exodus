d7f7db87a640257427b505e8efaff638e770c33f
Stephen <stephendpurdue@gmail.com>
2026-04-01 13:16:15 +0100

    Implemented TilemapVisualizer.

---
+++ Assets/_Scripts/TilemapVisualizer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class TilemapVisualizer : MonoBehaviour
+{
+    [SerializeField] 
+    private Tilemap floorTilemap;
+
+    // The tile that will be used to visualize the floor tiles on the tilemap.
+    private TileBase floorTile;
+
+    public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
+    {
+
+        PaintTiles(floorPositions, floorTilemap, floorTile);
+    }
+
+    private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile)
+    {
+        foreach (var position in positions)
+        {
+            PaintSingleTile(tilemap, tile, position);
+        }   
+    }
+
+    private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position)
+    {
+        var tilePosition = tilemap.WorldToCell((Vector3Int)position);
+        tilemap.SetTile(tilePosition, tile);
+    }
+}
