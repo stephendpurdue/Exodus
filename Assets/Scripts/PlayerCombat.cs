a031c949cf2e3a1af8447e74e852ce5bf19952c8
Stephen <stephendpurdue@gmail.com>
2026-04-10 10:36:38 +0100

    boilerplate attack code

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // Play Animation
+        // Detect enemies in range of attack
+        // Damage them
+    }
+}
