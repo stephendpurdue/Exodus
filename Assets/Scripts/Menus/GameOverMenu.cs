48a93ff4d092add32a275b065dddeb2c40478d94
Stephen <stephendpurdue@gmail.com>
2026-04-20 19:03:51 +0100

    docs: added further comments to GameOverMenu script

--- Assets/Scripts/Menus/GameOverMenu.cs
+++ Assets/Scripts/Menus/GameOverMenu.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    // This method is called when the "Restart" button is clicked.
+    // It sets the time scale to 1 (normal speed) and reloads the currently active scene.
     public void RestartButton()
     {
         Time.timeScale = 1f;
@@ -10,6 +12,7 @@ public class GameOverMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    // This method is called when the "Main Menu" button is clicked.
     public void MainMenuButton()
     {
         Time.timeScale = 1f;
