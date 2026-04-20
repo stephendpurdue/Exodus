8f15d49850c0458830c56450ea3e14ebe8ea9363
Stephen <stephendpurdue@gmail.com>
2026-04-20 11:52:14 +0100

    feature: implemented resolution dropdown functionality

--- Assets/Scripts/Menus/SettingsMenu.cs
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -2,22 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    // Populate the resolution dropdown menu with the available screen resolutions
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+
+    // Set the volume of the audio mixer based on the value of the slider
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("Volume", volume);
     }
 
+    // Set the quality level based on the index of the dropdown menu
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
+    // Set the fullscreen mode based on the value of the toggle
     public void SetFullscreen(bool isfullscreen)
     {
         Screen.fullScreen = isfullscreen;
