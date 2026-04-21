d62ac7947891d87e9ab5dac5bedb9ca6b4e2bf81
Stephen <stephendpurdue@gmail.com>
2026-04-21 09:18:04 +0100

    feature: finished CoinCollector logic

--- Assets/Scripts/Environment/CoinCollector.cs
+++ Assets/Scripts/Environment/CoinCollector.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinCollector : MonoBehaviour
 {
     private int Coin = 0;
+
+    public Text coinText;
+
+    private void Start()
+    {
+        // Automatically find the text in the scene when the player spawns
+        if (coinText == null)
+        {
+            GameObject textObj = GameObject.Find("CoinText"); 
+            if (textObj != null)
+            {
+                coinText = textObj.GetComponent<Text>();
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.tag == "Coin")
         {
             Coin++;
+            coinText.text = "Coin: " + Coin.ToString();
             Debug.Log(Coin);
             Destroy(other.gameObject);
         }
