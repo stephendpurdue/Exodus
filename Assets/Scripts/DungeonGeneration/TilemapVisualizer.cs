030206a2ce65a4389bc3117670e776d539cac532
Stephen <stephendpurdue@gmail.com>
2026-04-27 13:33:33 +0100

    refactor: refactored tilemapvisualizer to allow for more than one floor prefab

--- Assets/Scripts/DungeonGeneration/TilemapVisualizer.cs
+++ Assets/Scripts/DungeonGeneration/TilemapVisualizer.cs
@@ -11,24 +11,35 @@ public class TilemapVisualizer : MonoBehaviour
     [SerializeField]
     private Tilemap floorTilemap, wallTilemap;
 
-    // The tile that will be used to visualize the floor tiles on the tilemap.
+    // The tiles that will be used to visualize the floor on the tilemap.
     [SerializeField]
-    private TileBase floorTile, wallTop, wallSideRight, wallSideLeft, wallBottom, wallFull,
+    private TileBase floorTile;
+    [SerializeField]
+    private TileBase[] floorTilesPool;
+
+    [SerializeField]
+    private TileBase wallTop, wallSideRight, wallSideLeft, wallBottom, wallFull,
         wallInnerCornerDownLeft, wallInnerCornerDownRight, wallDiagonalCornerDownRight, wallDiagonalCornerDownLeft,
         wallDiagonalCornerUpRight, wallDiagonalCornerUpLeft;
 
     public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
     {
-
-        PaintTiles(floorPositions, floorTilemap, floorTile);
+        PaintTiles(floorPositions, floorTilemap);
     }
 
-    // Paints the given tiles on the tilemap at the specified positions. Loops t
-    private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile)
+    // Paints the given tiles on the tilemap at the specified positions.
+    private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap)
     {
         foreach (var position in positions)
         {
-            PaintSingleTile(tilemap, tile, position);
+            TileBase tileToPaint = floorTile;
+
+            if (floorTilesPool != null && floorTilesPool.Length > 0)
+            {
+                tileToPaint = floorTilesPool[UnityEngine.Random.Range(0, floorTilesPool.Length)];
+            }
+
+            PaintSingleTile(tilemap, tileToPaint, position);
         }
     }
 
